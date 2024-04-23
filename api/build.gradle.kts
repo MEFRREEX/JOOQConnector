42b29e7b228d124f1b6ab8e1e95f13c2fb10fe5c
MEFRREEX <mefrreex@gmail.com>
2024-04-23 14:59:07 +0300

    fix: api build

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jooq:jooq:3.19.7")
-    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
-    implementation("com.mysql:mysql-connector-j:8.3.0")
-    compileOnly("org.projectlombok:lombok:1.18.30")
+    api("org.jooq:jooq:3.19.7")
+    api("org.xerial:sqlite-jdbc:3.42.0.0")
+    api("com.mysql:mysql-connector-j:8.3.0")
+    compileOnlyApi("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
